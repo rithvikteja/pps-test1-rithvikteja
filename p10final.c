6541532c54102d80e81ae232eb19b51d9b291a51
Gayathri K <95470777+rithvikteja@users.noreply.github.com>
2022-02-09 07:42:42 +0000

    gayathri

--- p10final.c
+++ p10final.c
@@ -1,1 +1,32 @@
-
+#include<stdio.h>
+struct complex
+{
+  int real,imaginary;
+};
+struct complex input_chellox()
+{
+  struct complex p;
+  printf("Enter real and imaginary num\n");
+  scanf("%d %d", &p.real,&p.imaginary);
+  return p;
+}
+struct complex add_complex(struct complex a, struct complex b)
+{
+  struct complex x;
+  x.real=a.real+b.real;
+  x.imaginary=a.imaginary+b.imaginary;
+  return x;
+}
+void output(struct complex a,struct complex b, struct complex x)
+{
+  printf("the sum of (%d+%di)+(%d+%di)=(%d+%di)", a.real,a.imaginary,b.real,b.imaginary,x.real,x.imaginary);
+}
+int main()
+{
+  struct complex a,b,c;
+  a=input_chellox();
+  b=input_chellox();
+  c=add_complex(a,b);
+  output(a,b,c);
+  return 0;
+}
